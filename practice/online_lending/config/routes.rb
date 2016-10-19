72c9203549738ab6d7191aa93f4f459f34d0476f
Brian Cushman <cushman.brian@gmail.com>
2016-10-18 21:08:56 -0700

    added stuff

--- practice/online_lending/config/routes.rb
+++ practice/online_lending/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :sessions, only: [:create, :new, :destroy]
+  get 'online_lending/register', to: 'sessions#register'
+  get 'online_lending/logout', to: 'sessions#destroy'
+  get 'online_lending/login', to: 'sessions#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
